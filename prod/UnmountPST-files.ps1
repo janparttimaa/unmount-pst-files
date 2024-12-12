ffd20a9456c1ed7e69206ac75c6b62d24224b22a
Jan Parttimaa <31587976+janparttimaa@users.noreply.github.com>
2024-12-12 13:17:46 +0200

    Update UnmountPST-files.ps1

--- prod/UnmountPST-files.ps1
+++ prod/UnmountPST-files.ps1
@@ -1,5 +1,35 @@
-﻿# PowerShell equivalent of the VBScript code converted by AI
-# PowerShell-script unmounts PST-files from Classic Outlook
+<#
+.SYNOPSIS
+    Unmount PST-files from Microsoft Outlook Classic
+
+.DESCRIPTION
+    This PowerShell-script is forked from original VBS-script provided by Diane Poremsky. 
+    This script have been converted to PowerShell-format because Microsoft is deprecating VBScript.
+    Script run will happens per user context.
+    NOTE: This scripts have prerequirements that needs to be applied before deploying this script. Please check instructions from GitHub.
+
+.VERSION
+    1.0.0
+
+.AUTHOR
+    Converted to PowerShell: Jan Parttimaa (https://github.com/janparttimaa/apple-devices)
+    Original script and author: Diane Poremsky (https://www.slipstick.com/exchange/script-remove-pst-file-profile/)
+
+.COPYRIGHT
+    © 2018-2024 Diane Poremsky & Jan Parttimaa. All rights reserved.
+
+.LICENSE
+    This script is licensed under the MIT License.
+    You may obtain a copy of the License at https://opensource.org/licenses/MIT
+
+.RELEASE NOTES
+    1.0.0 - Initial release for PowerShell-format.
+
+.EXAMPLE
+    powershell.exe -ExecutionPolicy Bypass -File .\UnmountPST-files.ps1
+
+    This example is how to run this script running Windows PowerShell. Command needs to be run without admin rights on user context.
+#>
 
 # Suppress error messages
 $ErrorActionPreference = "SilentlyContinue"
