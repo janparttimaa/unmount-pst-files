7a658f186f4f351d8c987e1f92e913b1b04a2368
Jan Parttimaa <31587976+janparttimaa@users.noreply.github.com>
2024-12-12 11:26:04 +0200

    Add files via upload

---
+++ prod/Unmount_PST-files.ps1
@@ -0,0 +1,20 @@
+﻿# PowerShell equivalent of the VBScript code converted by AI
+# PowerShell-script unmounts PST-files from Classic Outlook
+
+# Suppress error messages
+$ErrorActionPreference = "SilentlyContinue"
+
+# Create an Outlook application object
+$objOutlook = New-Object -ComObject Outlook.Application
+
+# Get the list of Outlook stores
+$Stores = $objOutlook.Session.Stores
+
+# Loop through the stores
+for ($i = $Stores.Count; $i -ge 0; $i--) {
+    if ($Stores[$i].ExchangeStoreType -eq 3) {
+        # Remove the store if it is an Exchange store
+        $objFolder = $Stores[$i].GetRootFolder()
+        $objOutlook.Session.RemoveStore($objFolder)
+    }
+}
